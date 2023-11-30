495f2936914053206e8e565773bad33737967e22
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2023-11-30 21:27:28 +0100

    algorithm

--- nonstart.js
+++ nonstart.js
@@ -1,8 +1,14 @@
-String-1 -- nonStart
-Given 2 strings, return their concatenation, except omit the first char of each. The strings will be at least length 1.
+// String-1 -- nonStart
+// Given 2 strings, return their concatenation, except omit the first char of each. The strings will be at least length 1.
 
-Examples
+// Examples
 
-nonStart('Hello', 'There') → ellohere
-nonStart('java', 'code') → avaode
-nonStart('shotl', 'java') → hotlava
+// nonStart('Hello', 'There') → ellohere
+// nonStart('java', 'code') → avaode
+// nonStart('shotl', 'java') → hotlava
+
+function nonStart(a, b){
+    let removeFirsta= a.slice(1)
+    let removeFirstb= b.slice(1)
+    return removeFirsta + removeFirstb
+  }console.log(nonStart('Hello', 'There'));
