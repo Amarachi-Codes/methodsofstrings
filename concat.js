3d70195f54470c923962d3d431ee2fa571342ee0
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2023-11-30 22:18:12 +0100

    tried

--- concat.js
+++ concat.js
@@ -1,8 +1,16 @@
-String-1 -- conCat
-Given two strings, append them together (known as "concatenation") and return the result. However, if the concatenation creates a double-char, then omit one of the chars, so "abc" and "cat" yields "abcat".
+// String-1 -- conCat
+// Given two strings, append them together (known as "concatenation") and return the result. However, if the concatenation creates a double-char, then omit one of the chars, so "abc" and "cat" yields "abcat".
 
-Examples
+// Examples
 
-conCat('abc', 'cat') → abcat
-conCat('dog', 'cat') → dogcat
-conCat('abc', '') → abc
+// conCat('abc', 'cat') → abcat
+// conCat('dog', 'cat') → dogcat
+// conCat('abc', '') → 
+
+function conCat(a, b){
+    if(a.length-1 === b.charAt(0)){
+       let cutoff = b.slice(0)
+    return cutoff 
+  }
+//    return a.concat(b) 
+  }console.log(conCat('abc', 'cat'));
